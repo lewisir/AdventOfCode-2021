bd99fc98eda596bdcfcafcd26c200447e45febac
Ian Lewis <ian.r.lewis@bt.com>
2022-07-06 15:45:44 +0100

    Updates to Day 9 and Day 11 docstrings

--- Day12/aoc12.py
+++ Day12/aoc12.py
@@ -1,9 +1,25 @@
 # Advent of Code Day 12 - Passage Pathing
 
 import argparse
+import doctest
 
 def readFileData(filename):
-    """Function to read in data from an input file and return the processed file data"""
+    """
+    Function to read in data from an input file and return the processed file data
+
+    For a file containing:
+    start-A
+    start-b
+    A-c
+    A-b
+    b-d
+    A-end
+    b-end
+
+    The returned fileData shall be:
+    [['start', 'A'], ['start', 'b'], ['A', 'c'], ['A', 'b'], ['b', 'd'], ['A', 'end'], ['b', 'end']]
+
+    """
     fileData = []
     with open(filename) as file:
         for line in file:
@@ -13,21 +29,32 @@ def readFileData(filename):
 
 def createCaveGraph(caveConnections):
     """
-    Takes a list of connections and creates a disctionary with keys that are the nodes (caves) and the values are a list of connected nodes (caves)
+    Takes a list of connections and creates a dictionary
+    The keys that are the nodes (caves) and the values are a list of connected nodes (caves)
     It also ensures 'start' is never a destination and 'end' is never a source
     It duplicates entries to ensure bi-driectional connections between nodes (caves)
+
+    For this list of connections:
+    [['start', 'A'], ['start', 'b'], ['A', 'c'], ['A', 'b'], ['b', 'd'], ['A', 'end'], ['b', 'end']]
+
+    The following dictionary is returned:
+    {'start': ['A', 'b'], 'A': ['c', 'b', 'end'], 'c': ['A'], 'b': ['A', 'd', 'end'], 'd': ['b']}
+
+    >>> caveConnections = [['start', 'A'], ['b', 'start'], ['A', 'c'], ['A', 'b'], ['b', 'd'], ['A', 'end'], ['b', 'end']]
+    >>> print(createCaveGraph(caveConnections))
+    {'start': ['A', 'b'], 'A': ['c', 'b', 'end'], 'c': ['A'], 'b': ['A', 'd', 'end'], 'd': ['b']}
     """
     caveGraph = {}
     for connection in caveConnections:
         cave1 = connection[0]
         cave2 = connection[1]
-        if cave1 == 'end' or cave2 == 'start': # make sure start is at the start and end is at the end
+        if cave1 == 'end' or cave2 == 'start':  # make sure start is at the start and end is at the end
             cave1, cave2 = cave2, cave1
-        if cave1 not in caveGraph.keys(): # create en an edge in the graph
+        if cave1 not in caveGraph.keys():       # create en an edge in the graph
             caveGraph[cave1] = [cave2]
         else:
             caveGraph[cave1].append(cave2)
-        if cave1 == 'start' or cave2 == 'end': # duplicate the reverse entry if it doesn't contain start or end
+        if cave1 == 'start' or cave2 == 'end':  # duplicate the reverse entry if it doesn't contain start or end
             pass
         elif cave2 not in caveGraph.keys():
             caveGraph[cave2] = [cave1]
@@ -48,8 +75,77 @@ def printGraph(caveGraph):
     for cave in caveGraph:
         print(f"{cave}\t{caveGraph[cave]}")  
 
+def generatePaths(caveGraph,source,destination,allPaths,pathSoFar='',recursionLevel=0):
+    """
+    A recursive function to search paths and add completed paths to the allPaths list.
+
+    parameters:
+     caveGraph: A dictionary of the cave connections
+     source: A string containing the starting location (typically 'start')
+     destination: A string containing the end location (typically 'end')
+     allPaths: A list containing all of the paths discovered (each path is a string of caves)
+     pathSoFar: A string containg the current caves in the path (comma seaparetd caves)
+     recustionLevel: An integer to help debugging the recusrive function
+    
+    For the caveGraph
+    {'start': ['A', 'b'], 'A': ['c', 'b', 'end'], 'c': ['A'], 'b': ['A', 'd', 'end'], 'd': ['b']}
+    using 'start' and 'end' as the source and destintion
+    
+    The following paths are found:
+    ['start,A,c,A,b,A,end', 'start,A,c,A,b,end', 'start,A,c,A,end', 'start,A,b,A,c,A,end',
+     'start,A,b,A,end', 'start,A,b,end', 'start,A,end', 'start,b,A,c,A,end', 'start,b,A,end', 'start,b,end']
+
+    >>> caveGraph = {'start': ['A', 'b'], 'A': ['c', 'b', 'end'], 'c': ['A'], 'b': ['A', 'd', 'end'], 'd': ['b']}
+    >>> allPaths = []
+    >>> generatePaths(caveGraph,'start','end',allPaths)
+    >>> print(allPaths)
+    ['start,A,c,A,b,A,end', 'start,A,c,A,b,end', 'start,A,c,A,end', 'start,A,b,A,c,A,end', 'start,A,b,A,end', 'start,A,b,end', 'start,A,end', 'start,b,A,c,A,end', 'start,b,A,end', 'start,b,end']
+    """
+    recursionLevel += 1
+    if pathSoFar != '':
+        pathSoFar = pathSoFar + ',' + source
+    else:
+        pathSoFar = source
+    for node in caveGraph[source]:
+        if node == destination:
+            allPaths.append(pathSoFar + ',' + node)
+        elif node != 'start' and node in pathSoFar and node.islower():
+            pass
+        else:
+            generatePaths(caveGraph,node,destination,allPaths,pathSoFar,recursionLevel)
+
 def testCavePath(path,node,smCvLmt):
-    """Funciton to test whether the path so far could have the node added to it and not break the small cave visit limit"""
+    """
+    Funciton to test whether the path so far could have the cave added to it and not break the small cave visit limit
+
+    Parameters
+     path: A string of comma sepaarted nodes/caves
+     node: A string which is a cave
+     smCvLmt: An integer controlling the number of times a single small cave can be visited
+    
+    Returns
+        Boolean
+
+    The most times a singel cave appears in the path is recorded in teh maxSmCaveCount
+    If any small cave has reached the smCvLmt we only add a small cave if it's not already in the path
+    
+    >>> path = 'start,A,b,C,d'
+    >>> testCavePath(path,'A',2)
+    True
+    >>> testCavePath(path,'e',2)
+    True
+    >>> testCavePath(path,'b',2)
+    True
+    >>> testCavePath(path,'end',2)
+    True
+    >>> path = 'start,A,b,C,d,b'
+    >>> testCavePath(path,'b',2)
+    False
+    >>> testCavePath(path,'d',2)
+    False
+    >>> testCavePath(path,'F',2)
+    True
+    """
     pathList = path.split(',')
     maxSmCaveCount = 0
     for cave in pathList:
@@ -57,56 +153,58 @@ def testCavePath(path,node,smCvLmt):
             smCaveCount = pathList.count(cave)
         if smCaveCount > maxSmCaveCount:
             maxSmCaveCount = smCaveCount
-    #print(f"with path ({path}) and node ({node}) and limit {smCvLmt} ",end='')
-    if node.isupper() or node == 'start' or node == 'end':   # Big Caves, 'start' and 'end' are always OK to add. This leaves small caves to check
-        #print(f"True isupper(), 'start' or 'end'")
+    if node.isupper() or node == 'start' or node == 'end':   # Big Caves and 'start' and 'end' are always OK to add
         return True
-    elif node not in pathList:              # Small caves that have never been visited are always OK to add
-        #print(f"True new small cave for path")
+    elif node not in pathList:       # Small caves that have never been visited are always OK to add
         return True
-    elif pathList.count(node) >= smCvLmt:   # If the small cave already appears enough times to reach the limit then you can't add it again
-        #print(f"False count(node) >= limit")
-        return False
-    elif maxSmCaveCount >= smCvLmt:         # If any small cave has reached the limit, we don't visit any other small caves more than once
-        #print(f"False maxSmCaveCount {maxSmCaveCount} is >= limit")
+    elif maxSmCaveCount >= smCvLmt:  # If any small cave has reached the limit, we don't visit any other small caves more than once
         return False
     else:
-        #print(f"True - default")
         return True
-
-def generatePaths(caveGraph,source,destination,allPaths,pathSoFar='',recursionLevel=0):
-    """Function to search paths and add completed paths to the allPaths list."""
-    recursionLevel += 1
-    # recursionLevel is a way to display the print statements for debug and identify in which pass of the recursion each is occuring
-    #print(f"  " * recursionLevel + f" {recursionLevel} genPaths called with source {source} and current path {pathSoFar}")
-    if pathSoFar != '':
-        pathSoFar = pathSoFar + ',' + source
-    else:
-        pathSoFar = source
-    for node in caveGraph[source]:
-        #print(f"  " * recursionLevel + f"   {recursionLevel} Checking node {node}")
-        if node == destination:
-            allPaths.append(pathSoFar + ',' + node)
-            #print(f"  " * recursionLevel + f"   {recursionLevel}    found end and new path {allPaths[-1]} has been added")
-        elif node != 'start' and node in pathSoFar and node.islower():
-            pass
-        else:
-            generatePaths(caveGraph,node,destination,allPaths,pathSoFar,recursionLevel)
 
 def genPaths2(caveGraph,source,destination,allPaths,pathSoFar='',smCvLmt=1,recursionLevel=0):
-    """Function to search paths and add completed paths to the allPaths list. This includes a small cave limit allowing one small cave to be visited twice in a path"""
+    """
+    A recursive function to search paths and add completed paths to the allPaths list.
+    This includes a small cave limit allowing one small cave to be visited twice in a path
+
+    parameters:
+     caveGraph: A dictionary of the cave connections
+     source: A string containing the starting location (typically 'start')
+     destination: A string containing the end location (typically 'end')
+     allPaths: A list containing all of the paths discovered (each path is a string of caves)
+     pathSoFar: A string containg the current caves in the path (comma seaparetd caves)
+     smCvLmt: An integer controlling the number of times a single small cave can be visited
+     recustionLevel: An integer to help debugging the recusrive function
+    
+    For the caveGraph
+    {'start': ['A', 'b'], 'A': ['c', 'b', 'end'], 'c': ['A'], 'b': ['A', 'd', 'end'], 'd': ['b']}
+    using 'start' and 'end' as the source and destintion
+    
+    The following paths are found:
+    ['start,A,c,A,c,A,b,A,end', 'start,A,c,A,c,A,b,end', 'start,A,c,A,c,A,end', 'start,A,c,A,b,A,c,A,end',
+     'start,A,c,A,b,A,b,A,end', 'start,A,c,A,b,A,b,end', 'start,A,c,A,b,A,end', 'start,A,c,A,b,d,b,A,end',
+     'start,A,c,A,b,d,b,end', 'start,A,c,A,b,end', 'start,A,c,A,end', 'start,A,b,A,c,A,c,A,end', 
+     'start,A,b,A,c,A,b,A,end', 'start,A,b,A,c,A,b,end', 'start,A,b,A,c,A,end', 'start,A,b,A,b,A,c,A,end', 
+     'start,A,b,A,b,A,end', 'start,A,b,A,b,end', 'start,A,b,A,end', 'start,A,b,d,b,A,c,A,end', 
+     'start,A,b,d,b,A,end', 'start,A,b,d,b,end', 'start,A,b,end', 'start,A,end', 'start,b,A,c,A,c,A,end', 
+     'start,b,A,c,A,b,A,end', 'start,b,A,c,A,b,end', 'start,b,A,c,A,end', 'start,b,A,b,A,c,A,end', 
+     'start,b,A,b,A,end', 'start,b,A,b,end', 'start,b,A,end', 'start,b,d,b,A,c,A,end', 'start,b,d,b,A,end', 
+     'start,b,d,b,end', 'start,b,end']
+
+    >>> caveGraph = {'start': ['A', 'b'], 'A': ['c', 'b', 'end'], 'c': ['A'], 'b': ['A', 'd', 'end'], 'd': ['b']}
+    >>> allPaths = []
+    >>> genPaths2(caveGraph,'start','end',allPaths,'',2)
+    >>> print(allPaths)
+    ['start,A,c,A,c,A,b,A,end', 'start,A,c,A,c,A,b,end', 'start,A,c,A,c,A,end', 'start,A,c,A,b,A,c,A,end', 'start,A,c,A,b,A,b,A,end', 'start,A,c,A,b,A,b,end', 'start,A,c,A,b,A,end', 'start,A,c,A,b,d,b,A,end', 'start,A,c,A,b,d,b,end', 'start,A,c,A,b,end', 'start,A,c,A,end', 'start,A,b,A,c,A,c,A,end', 'start,A,b,A,c,A,b,A,end', 'start,A,b,A,c,A,b,end', 'start,A,b,A,c,A,end', 'start,A,b,A,b,A,c,A,end', 'start,A,b,A,b,A,end', 'start,A,b,A,b,end', 'start,A,b,A,end', 'start,A,b,d,b,A,c,A,end', 'start,A,b,d,b,A,end', 'start,A,b,d,b,end', 'start,A,b,end', 'start,A,end', 'start,b,A,c,A,c,A,end', 'start,b,A,c,A,b,A,end', 'start,b,A,c,A,b,end', 'start,b,A,c,A,end', 'start,b,A,b,A,c,A,end', 'start,b,A,b,A,end', 'start,b,A,b,end', 'start,b,A,end', 'start,b,d,b,A,c,A,end', 'start,b,d,b,A,end', 'start,b,d,b,end', 'start,b,end']
+    """
     recursionLevel += 1
-    # recursionLevel is a way to display the print statements for debug and identify in which pass of the recursion each is occuring
-    #print(f"  " * recursionLevel + f" {recursionLevel} genPaths called with source {source} and current path {pathSoFar}")
     if pathSoFar != '':
         pathSoFar = pathSoFar + ',' + source
     else:
         pathSoFar = source
     for node in caveGraph[source]:
-        #print(f"  " * recursionLevel + f"   {recursionLevel} Checking node {node}")
         if node == destination:
             allPaths.append(pathSoFar + ',' + node)
-            #print(f"  " * recursionLevel + f"   {recursionLevel}    found end and new path {allPaths[-1]} has been added")
         elif testCavePath(pathSoFar,node,smCvLmt):
             genPaths2(caveGraph,node,destination,allPaths,pathSoFar,smCvLmt,recursionLevel)
 
@@ -120,6 +218,8 @@ if __name__ == "__main__":
     else:
         filename = "inputTest.txt"
     
+    doctest.testmod()
+
     caveConnections = readFileData(filename)
     caveGraph = createCaveGraph(caveConnections)
 
--- Day9/aoc09.py
+++ Day9/aoc09.py
@@ -1,11 +1,12 @@
 # Advent of Code Day 9 - Smoke Basin
 
 import argparse
+import doctest
 
-def readFileData(filename):
+def read_file_data(filename):
     """
     Function to read in data from an input file and return the list containing the file data
-    
+
     If the file contains the following:
     2199943210
     3987894921
@@ -13,19 +14,19 @@ def readFileData(filename):
     8767896789
     9899965678
 
-    The then returned fileDate is:
+    The then returned file_data is:
     ["2199943210","3987894921","9856789892","8767896789","9899965678"]
     """
-    fileData = []
-    with open(filename) as file:
+    file_data = []
+    with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
-            fileData.append(line.rstrip('\n'))
-    return fileData
+            file_data.append(line.rstrip('\n'))
+    return file_data
 
 def find_string_low_points(string):
     """
-    Funciton to find the low points in a string and return a disctionary with their positions and values
-    
+    Finds the low points in a string and returns a dictionary with their positions and values
+
     A low point is a number in the string that is less than the values adjacent to it
 
     For example, in the string '8767896789', the returned dictionary is
@@ -33,6 +34,11 @@ def find_string_low_points(string):
         2: '6',
         6: '6'
     }
+
+    >>> print(find_string_low_points('8767896789'))
+    {2: '6', 6: '6'}
+    >>> print(find_string_low_points('2199943210'))
+    {1: '1', 9: '0'}
     """
     low_points = {}
     for index, char in enumerate(string):
@@ -48,18 +54,29 @@ def find_string_low_points(string):
     return low_points
 
 def increment_list_values(input_list,increment):
-    """Function increments each value in the list by the 'increment' parameter"""
-    input_list = [int(x) for x in input_list]
-    for x in range(len(input_list)):
-        input_list[x] = input_list[x] + increment
+    """
+    Function increments each value in the list by the 'increment' parameter
+    
+    >>> print(increment_list_values([1,2,3,4],1))
+    [2, 3, 4, 5]
+    >>> print(increment_list_values([5,0,1,4],2))
+    [7, 2, 3, 6]
+    """
+    input_list = [int(x)+increment for x in input_list]
     return input_list
 
 def get_adjacent_coordinates(height_map,coordinate):
     """
     At the given coordinate in the height_map return the adjacent coordinates
-    Adjacent coorcindates are to the left, right, above and below in the height_map
+    Adjacent coordindates are to the left, right, above and below in the height_map
+    
     For example, coordinate [4,2] has adajcent coordinates [4,1], [4,3], [5,2] and [6,2]
+    
     At the limits of the height_map then the coordinate may only have 2 or 3 adjacent coorindates
+
+    >>> height_map = ["2199943210","3987894921","9856789892","8767896789","9899965678"]
+    >>> print(get_adjacent_coordinates(height_map,[1,4]))
+    [[0, 4], [2, 4], [1, 3], [1, 5]]
     """
     new_coordinates = []
     if  coordinate[0] != 0:
@@ -80,10 +97,15 @@ def find_basin_coords(height_map,height,coordinate,basin_list):
     """
     Function is given a height_map, a height, a coordinate and a basin_list
     It returns a list containing all the coordinates of the basin
-    The basin_list reocrds all the coordinates of the current basin
+    The basin_list records all the coordinates of the current basin
     For the coordinate supplied, check the height_map to see if it's less than the height
     If it is, add it to the basin list then find the adjactent points
     The function is recusive, finding all coordinates that make up the basin
+
+    >>> height_map = ["2199943210","3987894921","9856789892","8767896789","9899965678"]
+    >>> basin_list = []
+    >>> find_basin_coords(height_map,9,[0,0],basin_list)
+    [[0, 0], [1, 0], [0, 1]]
     """
     if int(height_map[coordinate[0]][coordinate[1]]) < height:
         if coordinate not in basin_list:
@@ -93,20 +115,25 @@ def find_basin_coords(height_map,height,coordinate,basin_list):
                 find_basin_coords(height_map,height,coord,basin_list)
     return basin_list
 
-def list_product(integerList,number):
+def list_product(integer_list,number):
     """
     Given a list of integers and a number, multiply the number of largest integers together
-    For exampls, if the function is supplied this list:
+    For example, if the function is supplied this list:
     [2,5,3,1,7,4,9]
-    And the number 3, it will return the product of 9 x 7 x 5 (the three largest integers in the list)
+    And the number 3, it will return the product of 9x7x5 (the three largest integers in the list)
+
+    >>> list_product([2,5,3,1,7,4,9],3)
+    315
     """
-    integerList.sort(reverse = True)
+    integer_list.sort(reverse = True)
     product = 1
     for x in range(0,number):
-        product *= integerList[x]
+        product *= integer_list[x]
     return product
 
 if __name__ == "__main__":
+
+    doctest.testmod()
 
     # Handle command line argument for the input filename
     parser = argparse.ArgumentParser()
@@ -116,18 +143,18 @@ if __name__ == "__main__":
         filename = args.file
     else:
         filename = "Day9/inputTest.txt"
-    
-    height_map = readFileData(filename)
+
+    height_map = read_file_data(filename)
 
     # List allLowPoints will store values of each low point found in the height_map
     all_low_points = []
- 
+
     # Part 1 - find the low points in the height Map, increment each one by "1" and return the sum
     # Work through each line in the height_map, i is the index of the row in the height_map
     for i, value in enumerate(height_map):
         height_map_depth = len(height_map)
         row_low_points = find_string_low_points(value)
-        # Work through each low point found in the row and find the adjacent values in adjacent rows in the height_map
+        # For each low point found in the row and find the values in adjacent rows in the height_map
         # pos is the position of the low point found in the row
         for pos in row_low_points:
             if i == 0:                         # Case for the first row in the height_map
@@ -143,11 +170,9 @@ if __name__ == "__main__":
 
 
     # Part 2 - Finding the three largest basins in the map and multiply them together
-    """
-    Keep track of all the coordinates that form basins in all_basin_coordinates
-    This is done to prevent us from re-discovering the same basins as we work through the height_map
-    We find a basin by calling find_basin_coords and record the size of each basin in basin_sizes
-    """
+    # Keep track of all the coordinates that form basins in all_basin_coordinates
+    # This is done to stop us from re-discovering the same basins as we work through the height_map
+    # We find a basin by calling find_basin_coords and record the size of each basin in basin_sizes
     all_basin_coordinates = []
     basin_sizes = []
     for y, row in enumerate(height_map):
@@ -159,6 +184,6 @@ if __name__ == "__main__":
                 basin_size = (len(basin))
                 if basin_size > 0:
                     basin_sizes.append(basin_size)
-    
+
     answer = list_product(basin_sizes,3)
     print(f"Part 2 answer {answer}")
